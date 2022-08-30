efacebff3f7ff8db8f5899ad2db133f63480e434
Ilya <kucerubov365@gmail.com>
2022-08-30 12:28:14 +0300

    hm-8

---
+++ index.js
@@ -0,0 +1,36 @@
+const store = {
+    name: 'CumStore',
+    address: {
+        country: 'Ukrain',
+        city: 'Oddessa',
+        street: 'MaloArnautskaya',
+        houseNumber: 18
+    }
+}
+
+const clients = {};
+
+clients['Nataliia Kulybina'] = {
+    quantity: 10,
+    bonuses: 780
+}
+
+clients['Yuriy Kruchinin'] = {
+    quantity: 5,
+    bonuses: 30
+}
+
+clients['Oleksii Medvediev'] = {
+    quantity: 7,
+    bonuses: 1010
+}
+
+clients['Mykola Prymachok'] = {
+    quantity: 1,
+    bonuses: 500
+}
+
+clients['Bogdan Onatsky'] = {
+    quantity: 15,
+    bonuses: 5
+}
